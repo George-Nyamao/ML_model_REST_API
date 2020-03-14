b69e5796e3a3ea69552bf4e9c836f1e37a87e33d
Morara2017 <gmnyamao@hotmail.com>
2020-03-14 15:37:56 -0400

    add model, util,...

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## ML_model_REST_API
 
-This repository is a simple exercise in deploying a Machine Learning model as a REST API.
+This repository is a simple exercise in deploying a machine learning model as a REST API using Flask RESTful.
+
+The model is a simple Naives Bayes classifier that predicts the sentiment of phrases found in movie reviews.
+
+The data is taken from the Kaggle competition, Sentiment Analysis on Movie Reviews.
+
+The tutorial is by Nguyen Ngo and can be found at the following Medium article:
+
+https://towardsdatascience.com/deploying-a-machine-learning-model-as-a-rest-api-4a03b865c166
---
+++ model.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 14:17:56 2020
+
+@author: gmnya
+"""
+
+# Import libraries
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from util import plot_roc
+import pickle
+
+
+class NLPModel(object):
+    
+    def __init__(self):
+        self.clf = MultinomialNB()
+        self.vectorizer = TfidfVectorizer()
+        
+    def vectorizer_fit(self, X):
+        self.vectorizer.fit(X)
+        
+    def vectorizer_transform(self, X):
+        X_transformed = self.vectorizer.transform(X)
+        return X_transformed
+    
+    def train(self, X, y):
+        self.clf.fit(X,y)
+        
+    def predict_proba(self, X):
+        y_proba = self.clf.predict_proba(X)
+        return y_proba[:, 1]
+    
+    def predict(self, X):
+        y_pred = self.clf.predict(X)
+        return y_pred
+    
+    def pickle_vectorizer(self, path='models/TFIDFVectorizer.pkl'):
+        """
+        Save the trained vectorizer for future use.
+        """
+        with open(path, 'wb') as f:
+            pickle.dump(self.vectorizer, f)
+            print("Pickled classifier at {}".format(path))
+            
+    def plot_roc(self, X, y, size_x, size_y):
+        """ Plot the ROC curve for X_test and y_test."""
+        plot_roc(self.clf, X, y, size_x, size_y)
